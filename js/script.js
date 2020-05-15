55e5e864f4e0e674e82e3a881c9f44806333c79b
municiii <muisyojoy@gmail.com>
2020-05-15 10:56:50 +0300

    add new files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".A,.moja").click(function() {
+      $(".A,.moja").toggle();
+      $(".A,.moja").css("text-align","center")
+    })
+    $(".B,.mbili").click(function() {
+      $(".B,.mbili").toggle();
+      $(".B,.mbili").css("text-align","center")
+    })
+    $(".C,.tatu").click(function() {
+      $(".C,.tatu").toggle();
+      $(".C,.tatu").css("text-align","center")
+    })
+  })
